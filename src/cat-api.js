6c79d291ad4f4c10f5b42ebaea4d28c709201e5d
Amaryann <121064694+Amaryann@users.noreply.github.com>
2023-06-01 20:57:05 +0300

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,4 +1,4 @@
-const url = `https://api.thecatapi.com/v1/breed1212s`;
+const url = `https://api.thecatapi.com/v1/breeds`;
 const apiKey = "live_2wK86qLcZSv4Gz9hc07XayVt9dghXFN2zf0DhaGpB2GLRx8k0UhcyBDp8424aGyK"
 
 export const fetchBreeds = () => {
